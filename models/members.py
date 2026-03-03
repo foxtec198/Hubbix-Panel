6bff41f010dfbb875bb668b1304be1ff0b5cb7b0
Guilherme Breve <foxtec198@gmail.com>
2026-03-03 10:25:49 -0300

    Change de join date column for DateTime

--- models/members.py
+++ models/members.py
@@ -6,11 +6,17 @@ class Member(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    join_date = db.Column(db.Date, nullable=False)
+    join_date = db.Column(db.DateTime, nullable=False)
     active = db.Column(db.Boolean, default=True)
     last_login = db.Column(db.DateTime, nullable=True)
     partnership_id = db.Column(db.Integer)
+    hash = db.Column(db.String, nullable=False)
 
     @classmethod
     def _search_by_id(cls, id):
         return cls.query.filter(cls.id == id).first()
+
+    @classmethod
+    def _search_by_email(cls, mail):
+        return cls.query.filter(cls.email == mail).first()
+
