d29e20ff620579ce8fdcf7edf41faec776148f62
Guilherme Breve <foxtec198@gmail.com>
2026-04-03 01:20:48 -0300

    Nginx Config

--- services/nginx_server.py
+++ services/nginx_server.py
@@ -14,7 +14,7 @@ class NginxServer():
                 cmd = [
                     "/home/guibs/panel/venv/bin/certbot",
                     "certonly", "--dns-cloudflare", "--dns-cloudflare-credentials", 
-                    "/home/guibs/.secrets/certbot/cloudflare.ini",
+                    "--keep-until-expiring", "--staging", "/home/guibs/.secrets/certbot/cloudflare.ini",
                     "-d", client.custom_domain, "-d", f"www.{client.custom_domain}"
                 ]
                 run(cmd)
