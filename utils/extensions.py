6b74198ff7d45c9c62468f5220c7c12446cef446
Guilherme Breve <foxtec198@gmail.com>
2026-05-08 06:00:56 -0300

    Remove async mode

--- utils/extensions.py
+++ utils/extensions.py
@@ -1,7 +1,4 @@
 from flask_socketio import SocketIO
 
 # Cria o servidor de WebSockets
-ws = SocketIO(
-    cors_allowed_origins="*", 
-    async_mode="eventlet"
-) 
+ws = SocketIO(cors_allowed_origins="*")
