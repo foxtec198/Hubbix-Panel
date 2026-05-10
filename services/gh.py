c5a04f5ffd046f3b32106f586aaf4cb2a9a7f0c0
Guilherme Breve <foxtec198@gmail.com>
2026-05-10 13:10:56 -0300

    Fix: Dict wronged for KeyError 0

--- services/gh.py
+++ services/gh.py
@@ -3,6 +3,8 @@ from requests import get
 from functools import cache
 from datetime import datetime as dt
 from models.clients import Client
+from collections import defaultdict
+from dotenv import load_dotenv
 
 class GitHub:
     TOKEN = getenv("GH_TOKEN")
@@ -10,19 +12,23 @@ class GitHub:
     REPO = getenv("REPO")
     URL = f"https://api.github.com/repos/{OWNER}/{REPO}/commits"
 
-    @cache
+    def __init__(self):
+        load_dotenv
+        
     def get_commits(self, client_id, pagination=1):
         client = Client._search_by_id(int(client_id))
         if client:
             template_name = client.template
 
             # Customers Templates
+            templates = defaultdict()
             path = f"{self.URL}?path=templates/clients/{template_name}&per_page={pagination}"
             templates = get(path, headers={ "Authorization": f"Bearer {self.TOKEN}", "Accept": "application/vnd.github+json" })
             template_date = templates.json()[0]["commit"]["author"]["date"]
             template_date = dt.strptime(template_date, "%Y-%m-%dT%H:%M:%SZ")
 
             # Customers Static
+            static_date = defaultdict()
             path = f"{self.URL}?path=static/{template_name}&per_page={pagination}"
             static = get(path, headers={ "Authorization": f"Bearer {self.TOKEN}", "Accept": "application/vnd.github+json" })
             static_date = static.json()[0]["commit"]["author"]["date"]
