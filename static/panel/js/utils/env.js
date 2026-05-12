e6f768f45d70efe1385cec9040131a6b64c234c6
Guilherme Breve <foxtec198@gmail.com>
2026-05-12 19:41:28 -0300

    Change env

--- static/panel/js/utils/env.js
+++ static/panel/js/utils/env.js
@@ -2,6 +2,6 @@
 // =================== VARS =====================
 // ==============================================
 
-// export const api = "https://panel.lp.hubbix.com.br";
-export const api = "http://panel.localhost:5000";
+export const api = "https://panel.lp.hubbix.com.br";
+// export const api = "http://panel.localhost:5000";
 export const img = "https://api.hubbix.com.br/img/newFav.png";
